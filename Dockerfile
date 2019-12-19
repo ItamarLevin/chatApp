0f8c1722ff24a0d0cbcafa5845c31846fdbcdad1
ItamarLevin <Itamarlevin94@gmail.com>
2019-12-19 19:41:53 +0200

    adding a post, all functions works fine

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3.6-alpine
 
+#creating rooms folder
 WORKDIR /usr/src/app
-
+RUN mkdir rooms
 COPY requirements.txt ./
 COPY index.html ./
 RUN pip install --no-cache-dir -r requirements.txt
--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,32 @@
 from app import app
 from datetime import datetime
 import os.path
+from flask import request, render_template
 
 indexPath = "/usr/src/app/index.html"
-roomPath = "/home/itamar/usr/rooms/"
+roomPath = "/usr/src/app/rooms/"
 
 
 @app.route('/', methods=['GET'])
 def index_page():
     if os.path.isfile(indexPath):
         with open(indexPath, 'r') as f:
-            data = f.read()
+            main_page = f.read()
     else:
-        return "no logs available."
+        return render_template('404.html'), 404
 
-    return data
+    return main_page
 
 
 @app.route('/<room>', methods=['GET'])
 def room_page(room):
     if os.path.isfile(indexPath):
         with open(indexPath, 'r') as f:
-            data = f.read()
+            main_page = f.read()
     else:
-        return "no logs available."
+        return render_template('404.html'), 404
 
-    return data
+    return main_page
 
 
 @app.route('/api/chat/<room>', methods=['GET'])
@@ -34,7 +35,20 @@ def chat_room_page(room):
         with open(roomPath + room, 'r') as f:
             data = f.read()
     else:
-        return roomPath + room
-        # return "chat is currently empty."
+        return "Chat is currently empty."
 
     return data
+
+
+@app.route('/api/chat/<room>', methods=['POST'])
+def posting_api(room):
+    user_name = request.form["username"]
+    msg = request.form["msg"]
+
+    now = datetime.now()  # current date and time
+    formatted_time = now.strftime("[%x %X]")
+
+    with open(roomPath + room, 'a') as f:
+        f.write("{0} {1}: {2}\n".format(formatted_time, user_name, msg))
+    return ""
+
